132a2ce8ad995210778944b23efa3540b06bfbd5
limy <mingyuli1997@gmail.com>
2022-11-14 23:46:36 +0800

    refactor: 封装 axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,17 @@
-import axios, { type AxiosRequestConfig } from 'axios';
+import axios, {
+  type AxiosInstance,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from 'axios';
+
+interface MyAxiosRequestConfig<D> extends AxiosRequestConfig<D> {
+  /**
+   * 是否携带时间戳
+   * https://neteasecloudmusicapi.vercel.app/#/?id=%e8%b0%83%e7%94%a8%e5%89%8d%e9%a1%bb%e7%9f%a5
+   */
+  timestamp?: boolean;
+}
+
 export const DEFAULT_CONFIG = {
   // 基础url前缀
   baseURL: 'http://netease-cloud-music-api-lyart-rho.vercel.app/',
@@ -10,74 +23,102 @@ export const DEFAULT_CONFIG = {
   responseType: 'json',
 } as AxiosRequestConfig;
 
-const instance = axios.create(DEFAULT_CONFIG);
-
-instance.interceptors.request.use((config) => {
-  const { method, params } = config;
-  const headers: any = {};
-  if (method === 'get') {
-    // headers['Cache-Control'] = 'no-cache';
+class Http {
+  private instance: AxiosInstance;
+  constructor(config: AxiosRequestConfig) {
+    this.instance = axios.create(config);
+    this.initInterceptors();
   }
-  return {
-    ...config,
-    headers,
-  };
-});
 
-instance.interceptors.response.use(
-  (response) => {
-    let data;
-    // IE9时response.data是undefined，因此需要使用response.request.responseText(Stringify后的字符串)
-    if (response.data == undefined) {
-      data = JSON.parse(response.request.responseText);
-    } else {
-      data = response.data;
-    }
+  public get<T = any, R = AxiosResponse<T>, D = any>(
+    url: string,
+    config?: MyAxiosRequestConfig<D>
+  ): Promise<R> {
+    return this.instance.get(url, config as any);
+  }
 
-    return data;
-  },
-  (err) => {
-    if (err && err.response) {
-      switch (err.response.status) {
-        case 400:
-          err.message = '请求错误';
-          break;
-        case 401:
-          err.message = '未授权，请登录';
-          break;
-        case 403:
-          err.message = '拒绝访问';
-          break;
-        case 404:
-          err.message = `请求地址出错: ${err.response.config.url}`;
-          break;
-        case 408:
-          err.message = '请求超时';
-          break;
-        case 500:
-          err.message = '服务器内部错误';
-          break;
-        case 501:
-          err.message = '服务未实现';
-          break;
-        case 502:
-          err.message = '网关错误';
-          break;
-        case 503:
-          err.message = '服务不可用';
-          break;
-        case 504:
-          err.message = '网关超时';
-          break;
-        case 505:
-          err.message = 'HTTP版本不受支持';
-          break;
-        default:
+  private initInterceptors() {
+    const instance = this.instance;
+    instance.interceptors.request.use((config) => {
+      console.log(`[axios] config`, config);
+      const {
+        method,
+        timestamp = false,
+        params = {},
+      } = config as MyAxiosRequestConfig<any>;
+
+      if (timestamp) {
+        config.params = Object.assign(params, {
+          timestamp: new Date().getTime(),
+        });
       }
-    }
-    console.log(err);
-    return Promise.reject(err); // 返回接口返回的错误信息
-  }
-);
 
-export default instance;
+      const headers: any = {};
+      if (method === 'get') {
+        // headers['Cache-Control'] = 'no-cache';
+      }
+      return {
+        ...config,
+        headers,
+      };
+    });
+
+    instance.interceptors.response.use(
+      (response) => {
+        let data;
+        // IE9时response.data是undefined，因此需要使用response.request.responseText(Stringify后的字符串)
+        if (response.data == undefined) {
+          data = JSON.parse(response.request.responseText);
+        } else {
+          data = response.data;
+        }
+
+        return data;
+      },
+      (err) => {
+        if (err && err.response) {
+          switch (err.response.status) {
+            case 400:
+              err.message = '请求错误';
+              break;
+            case 401:
+              err.message = '未授权，请登录';
+              break;
+            case 403:
+              err.message = '拒绝访问';
+              break;
+            case 404:
+              err.message = `请求地址出错: ${err.response.config.url}`;
+              break;
+            case 408:
+              err.message = '请求超时';
+              break;
+            case 500:
+              err.message = '服务器内部错误';
+              break;
+            case 501:
+              err.message = '服务未实现';
+              break;
+            case 502:
+              err.message = '网关错误';
+              break;
+            case 503:
+              err.message = '服务不可用';
+              break;
+            case 504:
+              err.message = '网关超时';
+              break;
+            case 505:
+              err.message = 'HTTP版本不受支持';
+              break;
+            default:
+          }
+        }
+        console.log(err);
+        return Promise.reject(err); // 返回接口返回的错误信息
+      }
+    );
+  }
+}
+
+export default new Http(DEFAULT_CONFIG);
